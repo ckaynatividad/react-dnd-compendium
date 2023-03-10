cf0a7a34bfd69f36bb3de3e382563eae7e86da55
ckaynatividad <natividad.ckay@gmail.com>
2023-03-09 23:31:13 -0800

    made new file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Compendium from "./views/Compendium";
 import Home from "./views/Home";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/nekos" element={<Compendium />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
