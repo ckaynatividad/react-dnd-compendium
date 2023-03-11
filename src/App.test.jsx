571602c8651656fde7c584bba84b53750c65b095
ckaynatividad <natividad.ckay@gmail.com>
2023-03-11 04:33:39 -0800

    added test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("home page renders", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const home = screen.getByText(/home/i);
+  expect(home).toBeInTheDocument();
 });
