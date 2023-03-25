771261bab626ee056d97f454f99e3413f34112f8
ckaynatividad <natividad.ckay@gmail.com>
2023-03-25 00:04:39 -0700

    added races card component and extra variables into races

--- src/views/Race.jsx
+++ src/views/Race.jsx
@@ -1,18 +1,30 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import RacesCard from "../components/RacesCard";
 import { getRaces } from "../services/api";
 
 export default function Race() {
   const { race } = useParams();
   const [races, setRaces] = useState({});
+  const [lang, setLang] = useState([]);
+  const [ability, setAbility] = useState([]);
+  const [traits, setTraits] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       const data = await getRaces(race);
       setRaces(data);
+      setLang(data.languages);
+      setAbility(data.ability_bonuses);
+      setTraits(data.traits);
     };
     fetchData();
   }, [race]);
+
   // console.log(races);
-  return <div>{races.name}</div>;
+  return (
+    <div>
+      <RacesCard races={races} lang={lang} ability={ability} traits={traits} />
+    </div>
+  );
 }
