c2fe6760cc1a70d3ff6bc7acfd16694550e244fd
ckaynatividad <natividad.ckay@gmail.com>
2023-05-09 16:33:28 -0700

    added some basic happy tests

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,29 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "./App";
 
-test("home page renders", () => {
-  render(<App />);
-  const home = screen.getByText(/home/i);
-  expect(home).toBeInTheDocument();
+describe("happy tests", () => {
+  test("home page renders", async () => {
+    render(<App />);
+    const home = await screen.findByRole("heading", { name: /home/i });
+    expect(home).toBeInTheDocument();
+  });
+
+  test("nav renders", async () => {
+    render(<App />);
+    const nav = await screen.findByRole("navigation");
+    expect(nav).toBeInTheDocument();
+  });
+
+  test("can go to magic items", async () => {
+    render(<App />);
+    const link = await screen.findByRole("link", { name: /magic items/i });
+    userEvent.click(link);
+    const heading = await screen.findByRole("heading", {
+      name: /magic items/i,
+    });
+    const armor = await screen.findByText(/adamantine armor/i);
+    expect(heading).toBeInTheDocument();
+    expect(armor).toBeInTheDocument();
+  });
 });
