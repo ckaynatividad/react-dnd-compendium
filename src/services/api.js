03c6728ba6c95f74567f31b8996e5fe0081124f2
ckaynatividad <natividad.ckay@gmail.com>
2023-05-11 19:18:38 -0700

    made base api url in services into const variable for cleaner code

--- src/services/api.js
+++ src/services/api.js
@@ -1,45 +1,43 @@
+const BASE_API_PATH = "https://www.dnd5eapi.co/api";
+
 export async function getApi() {
-  const resp = await fetch("https://www.dnd5eapi.co/api/");
+  const resp = await fetch(`${BASE_API_PATH}`);
   const data = await resp.json();
   return data;
 }
 
 export async function getMagicItem(item = "") {
-  const resp = await fetch(`https://www.dnd5eapi.co/api/magic-items/${item}`);
+  const resp = await fetch(`${BASE_API_PATH}/magic-items/${item}`);
   const data = await resp.json();
   return item ? data : data.results;
 }
 
 export async function getClass(classItem = "") {
-  const resp = await fetch(`https://www.dnd5eapi.co/api/classes/${classItem}`);
+  const resp = await fetch(`${BASE_API_PATH}/classes/${classItem}`);
   const data = await resp.json();
   return classItem ? data : data.results;
 }
 
 export async function getSubclass(subclass = "") {
-  const resp = await fetch(
-    `https://www.dnd5eapi.co/api/subclasses/${subclass}`
-  );
+  const resp = await fetch(`${BASE_API_PATH}/subclasses/${subclass}`);
   const data = await resp.json();
   return subclass ? data : data.results;
 }
 
 export async function getProficiencies(prof = "") {
-  const resp = await fetch(`https://www.dnd5eapi.co/api/proficiencies/${prof}`);
+  const resp = await fetch(`${BASE_API_PATH}/proficiencies/${prof}`);
   const data = await resp.json();
   return prof ? data : data.results;
 }
 
 export async function getRaces(race = "") {
-  const resp = await fetch(`https://www.dnd5eapi.co/api/races/${race}`);
+  const resp = await fetch(`${BASE_API_PATH}/races/${race}`);
   const data = await resp.json();
   return race ? data : data.results;
 }
 
 export async function getEqCategories(category = "") {
-  const resp = await fetch(
-    `https://www.dnd5eapi.co/api/equipment-categories/${category}`
-  );
+  const resp = await fetch(`${BASE_API_PATH}/equipment-categories/${category}`);
   const data = await resp.json();
   return category ? data : data.results;
 }
